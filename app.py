0d7df753dba59cf530ecbe5ab0e02968e359f150
Darien Maynard <darienmaynard@gmail.com>
2023-01-27 14:44:56 -0500

    Uploaded function 2

--- app.py
+++ app.py
@@ -9,6 +9,21 @@ def parallel(arr):
     result = sum ** -1
     return result
 
-#Example of function
+#Example of function use
 things = [330,1000,2200]
 print(round(parallel(things), 3),"ohms")
+
+
+#Function 2 - Calculating the voltage drop across resistors in series
+def potential_divider(v,arr):
+    total_resistance = sum(arr)
+    voltage_drops = []
+    for r in arr:
+        voltage_drops = (r / total_resistance) * v
+        print (voltage_drops,"v")
+
+
+#Example of function use
+resistors = [3000,1000]
+potential_divider(9,resistors)
+
